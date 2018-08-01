25058a190528af83759f397ba43cfeb522c9acee
CoderGirlBre <41300229+CoderGirlBre@users.noreply.github.com>
2018-07-31 22:11:35 -0500

    Add files via upload

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -6,7 +6,17 @@ namespace Cars
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter the year of your first car: ");
+            string year = Console.ReadLine();
+
+            Console.Write("Enter the make of your first car: ");
+            string make = Console.ReadLine();
+
+            Console.Write("Enter the model name of your first car: ");
+            string model = Console.ReadLine();
+
+            Console.WriteLine("Your first car was a " + year + " " + make + " " + model + ".");
+            Console.ReadLine();
         }
     }
 }
